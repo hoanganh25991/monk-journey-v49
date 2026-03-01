a3ee05833c4f4e38f2640965632e96648e22b332
Anh Le <lehoanganh25991@gmail.com>
2026-03-01 22:56:01 +0700

    chore: auto commit 20260301-225601

--- js/config/version.js
+++ js/config/version.js
@@ -7,7 +7,7 @@
  */
 
 /** @type {string|null} Override with build-time value, or null for runtime */
-export const SOURCE_VERSION = '20260301T214503';
+export const SOURCE_VERSION = '20260301T225600';
 
 /**
  * Get the current source code version in YYYYMMDDTHHMMSS format.
